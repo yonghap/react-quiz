b31c7c7c64fbeebe5ae75d80a65f491b6db52fe1
Park Yong Ha <yonghapk@gmail.com>
2025-05-28 23:03:19 +0900

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,14 +38,6 @@ export default function Home() {
               </button>
             </Link>
           </li>  
-          <li className="w-[25%]">
-            <Link href="/quiz">
-              <button type="button">
-                <img src={IconGameHan.src} alt="IconGameHan" />
-                <span className="block mt-2 text-xs">한문 맞추기</span>
-              </button>
-            </Link>
-          </li>  
         </ul>
       </div>
     </main>
