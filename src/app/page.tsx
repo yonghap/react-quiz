7acc3d128979cb13458112e7cbfd00776b44b060
yonghap <box@an-d.kr>
2025-05-28 22:44:11 +0900

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { useEffect } from "react";
 import Link from "next/link";
 import { useQuizStore } from 'src/store/quiz';
 import IconGameNation from '@/assets/images/icon_game_nation.svg'
+import IconGameHan from '@/assets/images/icon_game_han.png'
 
 export default function Home() {
   const { reset } = useQuizStore();
@@ -12,7 +13,7 @@ export default function Home() {
   return (
     <main id="main">        
       <div className="p-6">
-        <ul className="flex gap-3">
+        <ul className="flex gap-8">
           <li className="w-[25%]">
             <Link href="/quiz">
               <button type="button">
@@ -20,7 +21,31 @@ export default function Home() {
                 <span className="block mt-2 text-xs">나라 맞추기</span>
               </button>
             </Link>
-          </li>        
+          </li>  
+          <li className="w-[25%]">
+            <Link href="/quiz">
+              <button type="button">
+                <img src={IconGameHan.src} alt="IconGameHan" />
+                <span className="block mt-2 text-xs">한문 맞추기</span>
+              </button>
+            </Link>
+          </li>
+          <li className="w-[25%]">
+            <Link href="/quiz">
+              <button type="button">
+                <img src={IconGameHan.src} alt="IconGameHan" />
+                <span className="block mt-2 text-xs">한문 맞추기</span>
+              </button>
+            </Link>
+          </li>  
+          <li className="w-[25%]">
+            <Link href="/quiz">
+              <button type="button">
+                <img src={IconGameHan.src} alt="IconGameHan" />
+                <span className="block mt-2 text-xs">한문 맞추기</span>
+              </button>
+            </Link>
+          </li>  
         </ul>
       </div>
     </main>
