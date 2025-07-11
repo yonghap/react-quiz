d75e4546e4b31f4032d9888b1be8c517e87905d7
Park Yong Ha <yonghapk@gmail.com>
2025-07-11 20:36:59 +0900

    add hanaja

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -77,25 +77,53 @@ export default function Quiz() {
 			setQuizIndex(quizIndex+1)
 		}
 	}
-	return (
-		<div>
-			<div className="p-4 text-center text-gray-500">
-				<div className="mb-2 text-xs">
-					<strong>{quizIndex}</strong> / {COMMON_CODE.QUIZ_COUNT}
-				</div>
-				<div className="mb-8 pt-[55%] bg-size-[100%_100%] border border-slate-200" style={{ backgroundImage: `url(${quizData.selected.download_url})` }} />
-				<ul>
-					{quizData.shuffled.map((i, idx) => {
-						return (
-							<div key={i.country_eng_nm}>
-								<button type="button" className={`block w-full my-4 text-2xl text-center`} onClick={() => handleClick(i.country_eng_nm)}>
-									{i.country_nm}
-								</button>
-							</div>
-						)
-					})}
-				</ul>
+
+// 나라 맞추기 퀴즈
+function renderCountryQuiz(quizData) {
+  return (
+    <div>
+      <div className="mb-8 pt-[55%] bg-size-[100%_100%] border border-slate-200" style={{ backgroundImage: `url(${quizData.selected.download_url})` }} />
+      <ul>
+        {quizData.shuffled.map((i) => (
+          <div key={i.country_eng_nm}>
+            <button onClick={() => handleClick(i.country_eng_nm)}>{i.country_nm}</button>
+          </div>
+        ))}
+      </ul>
+    </div>
+  );
+}
+// 한자 퀴즈
+function renderHanjaQuiz(quizData) {
+  return (
+    <div>
+      <div className="mb-8">
+				{quizData.selected.hanja}
+			</div>
+      <ul>
+        {quizData.shuffled.map((i) => (
+          <div key={i.country_eng_nm}>
+            <button onClick={() => handleClick(i.meaning)}>{i.meaning}</button>
+          </div>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+const renderByType = {
+  country: renderCountryQuiz,
+  hanja: renderHanjaQuiz,
+};
+const renderQuiz = renderByType[quizName];
+return (
+	<div>
+		<div className="p-4 text-center text-gray-500">
+			<div className="mb-2 text-xs">
+				<strong>{quizIndex}</strong> / {COMMON_CODE.QUIZ_COUNT}
 			</div>
 		</div>
-	)
+		{renderQuiz ? renderQuiz(quizData) : <p>퀴즈 타입이 올바르지 않습니다.</p>}
+	</div>
+)
 }
