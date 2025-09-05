f41e6c910f063c069694a6b912d283b20a6271aa
Park Yong Ha <yonghapk@gmail.com>
2025-09-05 20:58:11 +0900

    metadata 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,32 +1,29 @@
-'use client';
-
 import '@/styles/globals.css';
 import { ReactNode } from 'react';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Header from "@/components/navigation";
 import Footer from "@/components/footer";
-import logo from '@/assets/images/bg_main.png'
-const queryClient = new QueryClient();
-
+import QueryProvider from '@/components/QueryProvider'; // 클라이언트 컴포넌트
 
 export default function RootLayout({ children }: { children: ReactNode }) {
-	return (
-		<html lang="ko">
-			<head>
+  return (
+    <html lang="ko">
+      <head>
         <meta
           name="viewport"
-          content="initial-scale=1.0; maximum-scale=1.0; minimum-scale=1.0; user-scalable=no;"
+          content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no"
         />
+        <title>요덜이의 Next.js 앱</title>
+        <meta name="description" content="App Router 구조에서 메타데이터 설정 예제" />
       </head>
-			<body className="bg-gray-200">
-				<div className="overflow-hidden min-h-screen max-w-[600px] m-auto bg-white font-pretendard">
-					<Header></Header>
-					<QueryClientProvider client={queryClient}>
-						{children}
-					</QueryClientProvider>
-					<Footer></Footer>
-				</div>
-			</body>
-		</html>
-	);
+      <body className="bg-gray-200">
+        <div className="overflow-hidden min-h-screen max-w-[600px] m-auto bg-white font-pretendard">
+          <Header />
+          <QueryProvider>
+            {children}
+          </QueryProvider>
+          <Footer />
+        </div>
+      </body>
+    </html>
+  );
 }
