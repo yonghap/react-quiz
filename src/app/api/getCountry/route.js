861536b24922755a6b33dad12c218bc7e88c006d
Park Yong Ha <yonghapk@gmail.com>
2025-09-17 21:05:21 +0900

    test api

--- src/app/api/getCountry/route.js
+++ src/app/api/getCountry/route.js
@@ -8,13 +8,14 @@ export async function GET(request) {
 
 			while(page < 4) {
 				const query = new URLSearchParams({
-						serviceKey: apiKey,
+						serviceKey: 'hhPRU4TihqC7sGrFL7uNTmty4I7Hng2A57yNkCPaRsb+bnlxyetnLDADCFy/Dh0KshzZmRBEyFO1VEMKNHeuPg==',
 						pageNo: page,
 						numOfRows: 100,
 						returnType: 'JSON'
 				});
 
 				const res = await fetch(`http://apis.data.go.kr/1262000/CountryFlagService2/getCountryFlagList2?${query}`);
+				console.log('res', res)
 				if (!res.ok) {
 						console.log("API 오류 발생:", await res.text());
 						throw new Error(`API 요청 실패 - HTTP 상태 코드: ${res.status}`);
