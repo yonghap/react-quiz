78019db68a65764697da0d06e605bab5369ab832
Park Yong Ha <yonghapk@gmail.com>
2025-11-06 22:38:22 +0900

    add

--- src/constants/guideText.ts
+++ src/constants/guideText.ts
@@ -2,7 +2,7 @@ import { QuizName } from "@/types/quiz";
 
 export const guideText: Record<QuizName, string> = {
   country: "나라를 맞춰보세요!",
-  hanja: "무슨 뜻일까요?",
+  hanja: "무슨 뜻일까요!?",
   capital: "수도는 어디일까요?",
   sense: "정답을 맞춰보세요!",
 };
